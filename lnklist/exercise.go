d0b08de22fef87d48c5381d992c77ba85af9f29f
Hamza El <hamza.elbouatmani@gmail.com>
2020-05-05 23:47:07 +0000

    Format

--- lnklist/exercise.go
+++ lnklist/exercise.go
@@ -2,10 +2,9 @@ package lnklist
 
 import "strings"
 
-
 // Type Definitions:
 
-type datatype string;
+type datatype string
 
 type LinkedList struct {
 	head *node
@@ -16,70 +15,69 @@ type node struct {
 	next *node
 }
 
-const errorVal datatype = "";
-
+const errorVal datatype = ""
 
 // New creates a linked list with arbitrary number of initial values
 // Order of the values is taken into account.
 func New(vals ...datatype) (l *LinkedList) {
-	l = &LinkedList{};
-	var cur, n *node = nil, nil;
+	l = &LinkedList{}
+	var cur, n *node = nil, nil
 	for _, v := range vals {
-		n = &node {
+		n = &node{
 			data: v,
-		};
+		}
 		if cur == nil {
-			cur = n;
-			l.head = n;
+			cur = n
+			l.head = n
 		} else {
-			cur.next = n;
-			cur = cur.next;
+			cur.next = n
+			cur = cur.next
 		}
 	}
-	return l;
+	return l
 }
 
 // String prints the whole linked list X -> Y -> Z
 func (l *LinkedList) String() (s string) {
-	current := l.head;
-	s = "";
+	current := l.head
+	s = ""
 	for {
-		s += string(current.data);
+		s += string(current.data)
 		if current.next == nil {
-			s += "\n";
-			break;
+			s += "\n"
+			break
 		} else {
-			current = current.next;
-			s += " -> ";
+			current = current.next
+			s += " -> "
 		}
 	}
-	return strings.TrimSpace(s);
+	return strings.TrimSpace(s)
 }
 
 func (l *LinkedList) Length() int {
 	if l.head == nil {
-		return 0;
+		return 0
 	}
-	current := l.head;
-	length := 1;
+	current := l.head
+	length := 1
 	for current.next != nil {
-		current = current.next;
-		length++;
+		current = current.next
+		length++
 	}
-	return length;
+	return length
 }
 
 // Append adds a new node in the end of the linked list.
 func (l *LinkedList) Push(newData datatype) {
 
-	node_to_be_added := node {
-			data: newData,
-	};
+	node_to_be_added := node{
+		data: newData,
+	}
 
 	if l.head == nil { // Linked List is empty.
 		l.head = &node_to_be_added
 	} else {
-		current := l.head;
+		current := l.head
 		for current.next != nil {
 			current = current.next
 		}
@@ -90,144 +88,144 @@ func (l *LinkedList) Push(newData datatype) {
 
 // Contains searches for a value in each node of the linked list
 // and returns true if it finds it.
-func (l *LinkedList) Contains(value datatype) bool{
+func (l *LinkedList) Contains(value datatype) bool {
 	// If list is empty no point in searching.
 	if l.head == nil {
-		return false;
+		return false
 	}
 
-	current := l.head;
+	current := l.head
 	found := false
 	for {
 		if current.data == value {
-			found = true;
-			break;
+			found = true
+			break
 		} else {
 			if current.next != nil {
-				current = current.next;
+				current = current.next
 
 			} else {
-				break;
+				break
 			}
 		}
 	}
 
-	return found;
+	return found
 }
 
 // Pop returns the last node and deletes it from the list
 func (l *LinkedList) Pop() *node {
 	// If list is empty no point in searching.
 	if l.head == nil {
-		return nil;
+		return nil
 	}
 
-	var current *node = l.head;
-	var prev  *node = nil;
+	var current *node = l.head
+	var prev *node = nil
 	for current.next != nil {
-		prev = current;
-		current = current.next;
+		prev = current
+		current = current.next
 	}
-	prev.next = nil;
-	return current;
+	prev.next = nil
+	return current
 }
 
 // Count returns the number of times an element was found in the list
 func (l LinkedList) Count(val datatype) (c int) {
 	if l.head == nil {
-		return 0;
+		return 0
 	}
 
-	cur := l.head;
-	c = 0;
+	cur := l.head
+	c = 0
 	for {
 		if cur.data == val {
-			c++;
+			c++
 		}
 		if cur.next == nil {
-			break;
+			break
 		} else {
-			cur = cur.next;
+			cur = cur.next
 		}
 	}
-	return c;
+	return c
 }
 
 // GetNth returns the value stored in the nth index position
 func (l LinkedList) GetNth(n int) (val datatype, found bool) {
-	val = errorVal;
-	found = false;
+	val = errorVal
+	found = false
 	if l.head != nil {
-		cur := l.head;
-		i := 0;
+		cur := l.head
+		i := 0
 		for {
 			if i == n {
-				val = cur.data;
-				found = true;
-				break;
+				val = cur.data
+				found = true
+				break
 			} else {
 				if cur.next != nil {
-					cur = cur.next;
-					i++;
+					cur = cur.next
+					i++
 				} else {
-					break;
+					break
 				}
 			}
 		}
 	}
-	return val, found;
+	return val, found
 }
 
 // InsertNth inserts a node in the nth index position
 func (l *LinkedList) InsertNth(val datatype, n int) (done bool) {
-	done = false;
+	done = false
 	if l.head != nil {
 
-		node_to_be_inserted := &node {
+		node_to_be_inserted := &node{
 			data: val,
 		}
 
-		cur := l.head;
-		i := 0;
+		cur := l.head
+		i := 0
 		for {
 			if cur == nil {
-				break;
+				break
 			} else {
-				if i == (n-1) {
+				if i == (n - 1) {
 					if cur.next == nil {
-						cur.next = node_to_be_inserted;
-						done = true;
-						break;
+						cur.next = node_to_be_inserted
+						done = true
+						break
 					} else {
-						node_to_be_inserted.next = cur.next;
-						cur.next = node_to_be_inserted;
-						done = true;
-						break;
+						node_to_be_inserted.next = cur.next
+						cur.next = node_to_be_inserted
+						done = true
+						break
 					}
 				} else {
-					cur = cur.next;
-					i++;
+					cur = cur.next
+					i++
 				}
 			}
 		}
 
 	}
 
-	return done;
+	return done
 }
 
 // Append appends a list to the end of the calling list.
 func (l *LinkedList) Append(l2 *LinkedList) {
 	if l.head == nil {
-		l.head = l2.head;
+		l.head = l2.head
 	} else {
-		cur := l.head;
+		cur := l.head
 		for {
 			if cur.next == nil {
-				cur.next = l2.head;
-				break;
+				cur.next = l2.head
+				break
 			} else {
-				cur = cur.next;
+				cur = cur.next
 			}
 		}
 	}
@@ -236,40 +234,40 @@ func (l *LinkedList) Append(l2 *LinkedList) {
 // Reverse reverses a list
 func (l *LinkedList) Reverse() {
 	if l.head == nil || l.head.next == nil {
-		return;
+		return
 	}
 
-	var prev, tmp, cur *node = nil, nil, l.head;
-	for cur != nil{
+	var prev, tmp, cur *node = nil, nil, l.head
+	for cur != nil {
 		if cur.next == nil {
-			l.head = cur;
+			l.head = cur
 		}
-		tmp = cur.next;
-		cur.next = prev;
-		prev = cur;
-		cur = tmp;
+		tmp = cur.next
+		cur.next = prev
+		prev = cur
+		cur = tmp
 	}
 }
 
 // RemoveNth removes the node in position n
 func (l *LinkedList) RemoveNth(n int) {
 	if l.head == nil {
-		return;
+		return
 	}
 
-	var prev, cur *node = nil, l.head;
-	i := 0;
+	var prev, cur *node = nil, l.head
+	i := 0
 	for {
 		if i == n {
-			prev.next = cur.next;
-			break;
+			prev.next = cur.next
+			break
 		} else {
 			if cur.next == nil {
-				break;
+				break
 			} else {
-				prev = cur;
-				cur = cur.next;
-				i++;
+				prev = cur
+				cur = cur.next
+				i++
 			}
 		}
 	}
--- lnklist/exercise_test.go
+++ lnklist/exercise_test.go
@@ -2,24 +2,23 @@ package lnklist
 
 import "testing"
 
-
 func TestNew(t *testing.T) {
-	l := New("1", "2", "3");
-	if l.head.data != "1" || l.head.next.data !="2" || l.head.next.next.data != "3" {
-		t.Error("Error creating new Linked list with initial values");
+	l := New("1", "2", "3")
+	if l.head.data != "1" || l.head.next.data != "2" || l.head.next.next.data != "3" {
+		t.Error("Error creating new Linked list with initial values")
 	}
 }
 
 func TestString(t *testing.T) {
 
-	my_linked_list := LinkedList {
-		head : &node {
+	my_linked_list := LinkedList{
+		head: &node{
 			data: "1",
-			next: &node {
+			next: &node{
 				data: "2",
-				next: &node {
+				next: &node{
 					data: "3",
-					next: &node {
+					next: &node{
 						data: "4",
 					},
 				},
@@ -27,27 +26,27 @@ func TestString(t *testing.T) {
 		},
 	}
 
-	res := my_linked_list.String();
+	res := my_linked_list.String()
 	if res != "1 -> 2 -> 3 -> 4" {
-		t.Error("Error Formating Linked List to String");
+		t.Error("Error Formating Linked List to String")
 	}
 }
 
 func TestLength(t *testing.T) {
-	my_linked_list := LinkedList {
-		head: &node {
+	my_linked_list := LinkedList{
+		head: &node{
 			data: "1",
-			next: &node {
+			next: &node{
 				data: "2",
-				next: &node {
+				next: &node{
 					data: "3",
-					next: &node {
+					next: &node{
 						data: "4",
 					},
 				},
 			},
 		},
-	};
+	}
 
 	if my_linked_list.Length() != 4 {
 		t.Error("Error calculating Length of list!")
@@ -57,7 +56,7 @@ func TestLength(t *testing.T) {
 func TestPush(t *testing.T) {
 
 	// Create the Linked List
-	my_linked_list := LinkedList {}
+	my_linked_list := LinkedList{}
 
 	// Test append first element
 	my_linked_list.Push("Data of First node")
@@ -66,17 +65,17 @@ func TestPush(t *testing.T) {
 	}
 
 	// Test append second element
-	my_linked_list.Push("Data of First node");
+	my_linked_list.Push("Data of First node")
 	if my_linked_list.Length() != 2 {
 		t.Error("Error Pushing second element. Length after appending is", my_linked_list.Length())
 	}
 }
 func TestContains(t *testing.T) {
 
-	my_linked_list := LinkedList {
-		head : &node {
+	my_linked_list := LinkedList{
+		head: &node{
 			data: "1",
-			next: &node {
+			next: &node{
 				data: "2",
 				next: nil,
 			},
@@ -89,76 +88,75 @@ func TestContains(t *testing.T) {
 	if my_linked_list.Contains("22") != false {
 		t.Error("Found an element that does not exists.")
 	}
-	
+
 }
 
 func TestPop(t *testing.T) {
-	my_linked_list := LinkedList {
-		head : &node {
+	my_linked_list := LinkedList{
+		head: &node{
 			data: "1",
-			next: &node {
+			next: &node{
 				data: "2",
-				next: &node {
+				next: &node{
 					data: "3",
 					next: nil,
 				},
 			},
 		},
-	};
+	}
 
-	last := my_linked_list.Pop();
+	last := my_linked_list.Pop()
 	if last.data != "3" && my_linked_list.Length() == 2 {
 		t.Error("Error Poping last element")
 	}
 }
 
-
 func TestCount(t *testing.T) {
-	l := New("1", "2", "3", "4", "5", "2", "4", "2");
+	l := New("1", "2", "3", "4", "5", "2", "4", "2")
 	if l.Count("2") != 3 {
-		t.Error("Error Counting times element appears in list");
+		t.Error("Error Counting times element appears in list")
 	}
 }
 
 func TestGetNth(t *testing.T) {
-	l := New("1", "2", "3", "4", "5", "2", "4", "2");
-	ret, found := l.GetNth(4);
+	l := New("1", "2", "3", "4", "5", "2", "4", "2")
+	ret, found := l.GetNth(4)
 	if ret != "5" || found != true {
 		t.Error("Error Exercise GetNth")
 	}
 }
 
 func TestInsertNth(t *testing.T) {
-	l := New("1", "2", "3", "4", "5", "2", "4", "2");
-	oldLength := l.Length();
-	done := l.InsertNth("11", 3); // Insert "11" in the third index
+	l := New("1", "2", "3", "4", "5", "2", "4", "2")
+	oldLength := l.Length()
+	done := l.InsertNth("11", 3) // Insert "11" in the third index
 
 	if done != true || l.head.next.next.next.data != "11" || l.Length() != oldLength+1 {
 		t.Error("Error InsertNth ")
-	}	
+	}
 }
 
 func TestAppend(t *testing.T) {
-	l := New("1", "2", "3", "4", "5");
-	l2 := New("6", "7", "8");
-	l.Append(l2);
+	l := New("1", "2", "3", "4", "5")
+	l2 := New("6", "7", "8")
+	l.Append(l2)
 	if l.Length() != 8 {
-		t.Error("Error Exercise Append List");
+		t.Error("Error Exercise Append List")
 	}
 }
 
 func TestReverse(t *testing.T) {
-	l := New("1", "2", "3", "4");
-	l.Reverse();
+	l := New("1", "2", "3", "4")
+	l.Reverse()
 	if l.head.data != "4" || l.head.next.data != "3" || l.head.next.next.data != "2" || l.head.next.next.next.data != "1" {
-		t.Error("Error Exercise Reversing List");
+		t.Error("Error Exercise Reversing List")
 	}
 }
 
 func TestRemoveNth(t *testing.T) {
-	l := New("1", "2", "3", "4");
-	l.RemoveNth(1);
+	l := New("1", "2", "3", "4")
+	l.RemoveNth(1)
 	if l.head.next.data != "3" {
-		t.Error("Error Exercise Remove Nth");
+		t.Error("Error Exercise Remove Nth")
 	}
 }
