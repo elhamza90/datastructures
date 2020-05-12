b071a150dd7d6e37af29971f709eaf88699976de
Hamza El <hamza.elbouatmani@gmail.com>
2020-05-12 22:27:45 +0000

    Add DFS Traversal

--- graph/traversal.go
+++ graph/traversal.go
@@ -1,7 +1,10 @@
 package graph
 
 // Use our own implemented queue
-import "datastructures/queue";
+import (
+	"datastructures/queue"
+	"datastructures/stack"
+);
 
 func (g graph) BreadthFirstSearch(rootVertex int) (path []int){
 
@@ -23,5 +26,30 @@ func (g graph) BreadthFirstSearch(rootVertex int) (path []int){
 		}
 	}
 
+	return path;
+}
+
+
+func (g graph) DepthFirstSearch(rootVertex int) (path []int) {
+
+	// 1. Create and add root to stack
+	s := stack.New(rootVertex);
+	path = make([]int, 0, g.NbrOfVertices()); // result of traversal.
+	path = append(path, rootVertex);
+	visited := map[int]bool{rootVertex: true};
+
+	// 2. Loop
+	for s.Length() != 0 {
+		v, _ := s.Pop();
+		if visited[v] == false {
+			path = append(path, v);
+			visited[v] = true;
+		}
+		for _, adj := range g[v] {
+			if visited[adj] == false {
+				s.Push(adj);
+			}
+		}
+	}
 	return path;
 }
--- graph/traversal_test.go
+++ graph/traversal_test.go
@@ -1,16 +1,36 @@
 package graph
 
 import "testing"
-import "fmt"
 func TestBreadthFirstSearch(t *testing.T) {
-	g := New(7);
-	g.AddEdge(0, 4);
+	g := New(10);
+	g.AddEdge(3, 9);
+	g.AddEdge(3, 7);
+	g.AddEdge(7, 8);
 	g.AddEdge(0, 3);
 	g.AddEdge(0, 1);
 	g.AddEdge(1, 6);
 	g.AddEdge(1, 5);
 	g.AddEdge(1, 2);
 	res := g.BreadthFirstSearch(0);
-	fmt.Println(res);
+	if res[0] != 0 || res[1] != 3 || res[2] != 1 || res[3] != 9 || res[4] != 7 {
+		t.Error("Error BFS Graph traversal")
+	}
+	//fmt.Println("BFS", res);
 }
 
+func TestDepthFirstSearch(t *testing.T) {
+	g := New(10);
+	g.AddEdge(3, 9);
+	g.AddEdge(3, 7);
+	g.AddEdge(7, 8);
+	g.AddEdge(0, 3);
+	g.AddEdge(0, 1);
+	g.AddEdge(1, 6);
+	g.AddEdge(1, 5);
+	g.AddEdge(1, 2);
+	res := g.DepthFirstSearch(0);
+	if res[0] != 0 || res[1] != 1 || res[2] != 2 || res[3] != 5 || res[4] != 6 || res[5] != 3 || res[6] != 7 || res[7] != 8 {
+		t.Error("Error DFS Graph traversal")
+	}
+	//fmt.Println("DFS", res);
+}
